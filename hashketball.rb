f620b8ca9543cf83481c84f3c2e07ee532a2f267
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-04 17:05:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -96,6 +96,7 @@ def num_points_scored(player)
    game_hash.each do |location, teams|
     teams.each do |attribute, data|
       if data == team_name
+        teams.each do |a, b|
         binding.pry 
       end 
    end 
